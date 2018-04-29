515443ef0a268075523d418809ac3cd438702d5a
Gathros <6323830+Gathros@users.noreply.github.com>
2018-04-29 19:04:20 +0100

    Adding layers to GathVL

--- include/scene.h
+++ include/scene.h
@@ -7,25 +7,41 @@
 #include "color.h"
 #include "shapes.h"
 
+struct layer {
+    std::vector<shape*> shapes;
+
+    void draw(cairo_t *ctx);
+    void clear();
+};
+
 struct scene {
     vec size;
     color bg_clr;
-    std::vector<shape*> shape_vector;
+    std::vector<layer> layers;
 
-    scene() :
-            size({600, 400}), bg_clr({0.0, 0.0, 0.0, 1.0}) {}
+    scene() : size({600, 400}), bg_clr({0.0, 0.0, 0.0, 1.0}) {
+        layers.emplace_back(layer());
+    }
 
-    scene(color bg_color) :
-            size({600, 400}), bg_clr(bg_color) {}
+    scene(color bg_color) : size({600, 400}), bg_clr(bg_color) {
+        layers.emplace_back(layer());
+    }
 
-    scene(vec scene_size) :
-            size(scene_size), bg_clr({0.0, 0.0, 0.0, 1.0}) {}
+    scene(vec scene_size) : size(scene_size), bg_clr({0.0, 0.0, 0.0, 1.0}) {
+        layers.emplace_back(layer());
+    }
 
-    scene(vec scene_size, color bg_color) :
-            size(scene_size), bg_clr(bg_color) {}
+    scene(vec scene_size, color bg_color) : size(scene_size), bg_clr(bg_color) {
+        layers.emplace_back(layer());
+    }
 
     void draw(cairo_t *ctx);
-
+    void add_layer();
+    void add_layers(int num_layers);
+    void add_layer_at(int position);
+    void add_layers_from(int position, int num_layers);
+    void clear_layer(int layer_num);
+    void clear();
 };
 
 #endif //SCENE_H
--- src/scene.cpp
+++ src/scene.cpp
@@ -1,7 +1,46 @@
 #include "../include/scene.h"
+#include <iterator>
 
-void scene::draw(cairo_t *ctx) {
-    for (auto s : shape_vector) {
+void layer::draw(cairo_t *ctx) {
+    for (auto s : shapes) {
         s->draw(ctx, s);
     }
+}
+
+void layer::clear() {
+    shapes.clear();
+}
+
+void scene::draw(cairo_t *ctx) {
+    for (auto l : layers) {
+        l.draw(ctx);
+    }
+}
+
+void scene::add_layer() {
+    layers.push_back(layer());
+}
+
+void scene::add_layers(int num_layers) {
+    layers.insert(layers.end(), num_layers, layer());
+}
+
+void scene::add_layer_at(int position) {
+    auto iter = layers.begin();
+    std::advance(iter, position);
+    layers.insert(iter, layer());
+}
+
+void scene::add_layers_from(int position, int num_layers) {
+    auto iter = layers.begin();
+    std::advance(iter, position);
+    layers.insert(iter, num_layers, layer());
+}
+
+void scene::clear_layer(int layer_num) {
+    layers[layer_num].clear();
+}
+
+void scene::clear() {
+    layers.clear();
 }
