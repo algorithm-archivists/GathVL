6786c19ad82fefe3ffddc811367b3f1418d018a7
Gathros <6323830+Gathros@users.noreply.github.com>
2018-08-06 11:28:18 +0100

    Adding mac system files

---
+++ include/systems/mac.h
@@ -0,0 +1,10 @@
+#ifndef #GATHVL_MAC_H
+#define #GATHVL_MAC_H
+
+#include <string>
+#include <vector>
+
+int subprocess(const std::string& program,
+               const std::vector<std::string>& args);
+
+#endif //GATHVL_MAC_H
---
+++ src/systems/mac.cpp
@@ -0,0 +1,32 @@
+#include "../../include/systems/mac.h"
+
+#include <cstdlib>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <unistd.h>
+
+int subprocess(const std::string& program,
+               const std::vector<std::string>& args) {
+
+    std::vector<char *> argv;
+
+    for (auto& str : args) {
+        argv.push_back(&str.front());
+    }
+    argv.push_back(NULL);
+
+    pid_t pid = fork();
+    int status = 0;
+
+    if (pid == 0) {
+        execv(programi.data(), argv.data());
+        std::exit(127);
+    } else {
+        waitpid(pid, &status, 0);
+        if (WIFEXITED(status) && (WEXITSTATUS(status) == 0)) {
+            return 0;
+        }
+    }
+
+    return 1;
+}
